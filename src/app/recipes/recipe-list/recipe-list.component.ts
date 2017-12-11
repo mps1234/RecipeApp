5a2397be3067d9e8c2cd6acf0e99e6d957455f93
Mayur <mayur.pathak52@gmail.com>
2017-12-11 11:20:16 +0530

    recipe details component working dynamically, user can select recipe and show the details of it

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,13 +8,17 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 	recipes: Recipe[] = [
 		new Recipe('A Test Recipe', 'This is simply a test', 'https://googlecreativelab.github.io/coder/assets/images/coder_projects.jpg'),
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://googlecreativelab.github.io/coder/assets/images/coder_projects.jpg')
+		new Recipe('Another Test Recipe', 'This is simply a test', 'https://googlecreativelab.github.io/coder/assets/images/coder_projects.jpg')
 	];
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
